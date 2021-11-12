3c4c76931f50ba0aa2323f4c3f490432b3eb8962
Aditya Shinagari <ankushshinagari@gmail.com>
2021-11-12 18:11:04 +0530

    animation added

--- js/index.js
+++ js/index.js
@@ -2,6 +2,15 @@
 function clearcontent() {
   document.getElementById('generatedCss').innerHTML = "";
 }
+window.onload = function() {
+  setTimeout(removeLoader, 2000);
+  function removeLoader(){
+      document.getElementById('lottie').remove();
+    }
+  }
+
+
+
 document.getElementById("height").addEventListener("input", changeHeight);
 document.getElementById("width").addEventListener("input", changeWidth);
 document.getElementById("shadowX").addEventListener("input", changeShadowXY);
@@ -19,12 +28,160 @@ document.getElementById("borderRadiusTopRight").addEventListener("input", change
 document.getElementById("borderRadiusBottomLeft").addEventListener("input", changeBorderRadius);
 document.getElementById("borderRadiusBottomRight").addEventListener("input", changeBorderRadius);
 document.getElementById("rotate").addEventListener("input", rotate);
+document.getElementById("animationType").addEventListener("input", animateCard);
+document.getElementById("duration").addEventListener("input", animateCard);
+document.getElementById("iteration").addEventListener("input", animateCard);
+document.getElementById("animationTimingFunction").addEventListener("input", animateCard);
+document.getElementById("delay").addEventListener("input", animateCard);
 document.getElementById("animateStopBtn").addEventListener("click", stopAnimation);
+
+
+document.getElementById("spin").addEventListener("click", spinAnimation);
+document.getElementById("gelatine").addEventListener("click", gelatineAnimation);
+document.getElementById("pulse").addEventListener("click", pulseAnimation);
+document.getElementById("flash").addEventListener("click", flashAnimation);
+document.getElementById("notify").addEventListener("click", notifyAnimation);
+document.getElementById("elasticSpin").addEventListener("click", elasticSpinAnimation);
+document.getElementById("fadeIn").addEventListener("click", fadeInAnimation);
+document.getElementById("fadeOut").addEventListener("click", fadeOutAnimation);
+document.getElementById("fadeInDown").addEventListener("click", fadeInDownAnimation);
+document.getElementById("fadeOutDown").addEventListener("click", fadeOutDownAnimation);
+document.getElementById("fadeInLeft").addEventListener("click", fadeInLeftAnimation);
+document.getElementById("fadeOutRight").addEventListener("click", fadeOutRightAnimation);
 document.getElementById("bounce").addEventListener("click", bounceAnimation);
-// document.getElementById("cssBtn").addEventListener("click", openModal);
-function bounceAnimation(){
-  document.getElementById("create").style.animation = "mymove 1s infinite";
+document.getElementById("bounce2").addEventListener("click", bounce2Animation);
+document.getElementById("grow").addEventListener("click", growAnimation);
+document.getElementById("shrink").addEventListener("click", shrinkAnimation);
+document.getElementById("shake").addEventListener("click",  shakeAnimation);
+document.getElementById("swing").addEventListener("click", swingAnimation);
+document.getElementById("wobble").addEventListener("click", wobbleAnimation);
+document.getElementById("hinge").addEventListener("click", hingeAnimation);
+document.getElementById('playPause').addEventListener('click', changePlayPause);
+document.getElementById('repeat').addEventListener('change',function(){
+var checkboxVal = document.getElementById('repeat').checked;
+  if(checkboxVal == true){
+     document.getElementById('iteration').disabled  = true;
+  }else{
+    document.getElementById('iteration').disabled  = false;
+  }
+});
+
+function changePlayPause(){
+  const icon = this.querySelector('i');
+  // alert(icon);
+  const text = this.querySelector('span');
+  if (icon.classList.contains('fa-play')) {
+    icon.classList.remove('fa-play');
+    icon.classList.add('fa-pause');
+    document.getElementById('playPause').setAttribute('onclick','play()');
+    text.innerHTML = 'Hide';
+  } else {
+    icon.classList.remove('fa-pause');
+    icon.classList.add('fa-play');
+    document.getElementById('playPause').setAttribute('onclick','paused()') ;
+    text.innerHTML = 'Show';
+  }
 }
+function animateCard() {
+  const card = document.getElementById('create');
+  var checkboxValueVerify = document.getElementById('repeat').checked;
+  if(checkboxValueVerify){
+   card.style.animation = getAnimationName() + ' ' + getAnimationTime() + 'ms' + ' ' + getAnimationTimingFunction() + ' ' + getAnimationTimeDelay() + 'ms' + ' ' + 'infinite';
+ }else{
+   card.style.animation = getAnimationName() + ' ' + getAnimationTime() + 'ms' + ' ' + getAnimationTimingFunction() + ' ' + getAnimationTimeDelay() + 'ms' + ' ' + getIterationCount();
+ }
+}
+function play(){
+  const card = document.getElementById('create');
+  card.style.animationPlayState  = 'running';
+}
+function paused(){
+  const card = document.getElementById('create');
+  card.style.animationPlayState  = 'paused';
+}
+// document.getElementById("cssBtn").addEventListener("click", openModal);
+
+// to get animation delay time
+function getAnimationTimeDelay() {
+  return document.getElementById('delay').value;
+}
+// to get animation name
+function getAnimationName() {
+  return document.getElementById('animationType').value;
+}
+// to get animation time
+function getAnimationTime() {
+  return document.getElementById('duration').value;
+}
+// to get iteration time
+function getIterationCount() {
+  return document.getElementById('iteration').value;
+}
+// to get animation timing function
+function getAnimationTimingFunction() {
+  return document.getElementById('animationTimingFunction').value;
+}
+function gelatineAnimation(){
+  document.getElementById("create").style.animation = "gelatine 1s infinite";
+}
+function spinAnimation(){
+  document.getElementById("create").style.animation = "rot 1s infinite linear";
+}
+function pulseAnimation(){
+  document.getElementById("create").style.animation = "pulse 1s infinite ease-in-out alternate";
+}
+function flashAnimation(){
+  document.getElementById("create").style.animation = "flash 500ms ease infinite alternate";
+}
+function notifyAnimation(){
+  document.getElementById("create").style.animation = "hithere 1s ease infinite";
+}
+function elasticSpinAnimation(){
+  document.getElementById("create").style.animation = "elastic-spin 1s infinite ease";
+}
+function fadeInAnimation(){
+  document.getElementById("create").style.animation = "fade-in 2s linear infinite";
+}
+function fadeOutAnimation(){
+  document.getElementById("create").style.animation = "fade-out 2s linear infinite";
+}
+function fadeInDownAnimation(){
+  document.getElementById("create").style.animation = "fade-in-down 2s ease infinite";
+}
+function fadeOutDownAnimation(){
+  document.getElementById("create").style.animation = "fade-out-down 2s ease infinite";
+}
+function fadeInLeftAnimation(){
+  document.getElementById("create").style.animation = "fade-in-left 2s ease infinite";
+}
+function fadeOutRightAnimation(){
+  document.getElementById("create").style.animation = "fade-out-right 2s ease infinite";
+}
+function bounceAnimation(){
+  document.getElementById("create").style.animation = "bounce 2s ease infinite";
+}
+function bounce2Animation(){
+  document.getElementById("create").style.animation = "bounce2 2s ease infinite";
+}
+function growAnimation(){
+  document.getElementById("create").style.animation = "grow 2s ease infinite";
+}
+function shrinkAnimation(){
+  document.getElementById("create").style.animation = "shrink 2s ease infinite";
+}
+function shakeAnimation(){
+  document.getElementById("create").style.animation = "shake 2s ease infinite";
+}
+function swingAnimation(){
+  document.getElementById("create").style.animation = "swing 2s ease infinite";
+}
+function wobbleAnimation(){
+  document.getElementById("create").style.animation = "wobble 2s ease infinite";
+}
+function hingeAnimation(){
+  document.getElementById("create").style.animation = "hinge 2s ease infinite";
+}
+
 function copyDivToClipboard() {
      var range = document.createRange();
      range.selectNode(document.getElementById("generatedCss"));
@@ -250,8 +407,17 @@ function generateCss() {
   if (radioButton == 'outset') {
     radioButton = ' ';
   }
-  var arr = ['height', 'width', 'box-shadow', 'border-radius', 'background-color', 'border','transform'];
-  var arrValue = [height + 'px;', width + 'px;', radioButton + ' ' + boxShadowX + 'px' + ' ' + boxShadowY + 'px' + ' ' + blurValue() + 'px' + ' ' + shadowSpread() + 'px' + ' ' + boxShadowColor + '<input type="color" style="width:15px;height:15px" disabled value="' + boxShadowColor + '"/>;', borderRadiusTopLeft() + '%' + ' ' + borderRadiusTopRight() + '%' + ' ' + borderRadiusBottomLeft() + '%' + ' ' + borderRadiusBottomRight() + '%', bgColor + '<input type="color" style="width:15px;height:15px" disabled value="' + bgColor + '"/>;', borderWidth + 'px solid' + ' ' + borderColor + '<input type="color" style="width:15px;height:15px" disabled value="' + borderColor + '"/>;','rotate('+rotationValue()+'deg);'];
+  var animationName = document.getElementById('animationType').value;
+  var animationTimingFunction = document.getElementById('animationTimingFunction').value;
+  if(animationName == 1 || animationTimingFunction == 'notselected'){
+    var arr = ['height', 'width', 'box-shadow', 'border-radius', 'background-color', 'border','transform'];
+    var arrValue = [height + 'px;', width + 'px;', radioButton + ' ' + boxShadowX + 'px' + ' ' + boxShadowY + 'px' + ' ' + blurValue() + 'px' + ' ' + shadowSpread() + 'px' + ' ' + boxShadowColor + '<input type="color" style="width:15px;height:15px" disabled value="' + boxShadowColor + '"/>;', borderRadiusTopLeft() + '%' + ' ' + borderRadiusTopRight() + '%' + ' ' + borderRadiusBottomLeft() + '%' + ' ' + borderRadiusBottomRight() + '%', bgColor + '<input type="color" style="width:15px;height:15px" disabled value="' + bgColor + '"/>;', borderWidth + 'px solid' + ' ' + borderColor + '<input type="color" style="width:15px;height:15px" disabled value="' + borderColor + '"/>;','rotate('+rotationValue()+'deg);'];
+    console.log(document.getElementById('create').style);
+
+  }else{
+    var arr = ['height', 'width', 'box-shadow', 'border-radius', 'background-color', 'border','transform','animation'];
+    var arrValue = [height + 'px;', width + 'px;', radioButton + ' ' + boxShadowX + 'px' + ' ' + boxShadowY + 'px' + ' ' + blurValue() + 'px' + ' ' + shadowSpread() + 'px' + ' ' + boxShadowColor + '<input type="color" style="width:15px;height:15px" disabled value="' + boxShadowColor + '"/>;', borderRadiusTopLeft() + '%' + ' ' + borderRadiusTopRight() + '%' + ' ' + borderRadiusBottomLeft() + '%' + ' ' + borderRadiusBottomRight() + '%', bgColor + '<input type="color" style="width:15px;height:15px" disabled value="' + bgColor + '"/>;', borderWidth + 'px solid' + ' ' + borderColor + '<input type="color" style="width:15px;height:15px" disabled value="' + borderColor + '"/>;','rotate('+rotationValue()+'deg);',animationName + ' ' + getAnimationTime() + 'ms' + ' ' + getAnimationTimeDelay() + 'ms' + ' ' + animationTimingFunction + ';' ];
+  }
   var cont = document.getElementById('generatedCss');
   console.log(arrValue);
   // create ul element and set the attributes.
@@ -267,4 +433,5 @@ function generateCss() {
     ul.appendChild(li); // append li to ul.
   }
   cont.appendChild(ul);
+
 }
